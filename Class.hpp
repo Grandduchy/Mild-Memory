98f555150249b2e2aaee41b84182f6dd23ab8c3a
Grandduchy <jchallenger1@hotmail.com>
2017-06-21 18:35:26 -0400

    Version 2

---
+++ Class.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <random>
+#include "StandardEntries.hpp"
+using std::string;
+
+class DataEntries {
+	using entry_ptr = std::unique_ptr < const std::array<const char*, sESize >>;
+	using entry_array = std::array<const char*, sESize>;
+public:
+	DataEntries(const std::array<const char*, sESize>& arr) : entries(&arr) {};
+
+	void addEntries(std::vector<string>& vec);
+	std::vector<string> returnEntries(const int& i) const;
+	~DataEntries();
+private:
+	entry_ptr entries;
+	std::unique_ptr<std::vector<string>> new_entries;
+};
+
+void DataEntries::addEntries(std::vector<string>&vec) {
+	new_entries.reset(&vec);
+}
+std::vector<string> DataEntries::returnEntries(const int& entry_count) const {
+	std::default_random_engine eng(time(0));
+	std::uniform_int_distribution<int> roll(0, sESize  + ( !new_entries ? new_entries->size() :  0  )     );
+	std::vector<string> return_entry;
+	for (int x = entry_count; x != 0; x--) {
+		int rn = roll(eng);
+		try {
+			if (rn <= sESize) {
+				if (std::find(return_entry.begin(), return_entry.end(), entries->at(rn)) == return_entry.end())
+					return_entry.push_back(entries->at(rn));
+				else
+					x--;
+			}
+			else {
+				if (std::find(return_entry.begin(), return_entry.end(), new_entries->at(rn)) == return_entry.end())
+					return_entry.push_back(new_entries->at(rn));
+				else
+					x--;
+			}
+		}
+		catch (std::exception& e) { std::cout << e.what() << std::endl; }
+		
+	}
+	return return_entry;
+}
+
+DataEntries::~DataEntries() {
+	entries.release(); // dont delete static members 
+	new_entries.release(); // just remove the ownership
+}
